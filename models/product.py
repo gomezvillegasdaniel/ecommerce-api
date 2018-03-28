f7cc04882b5704b6db981aa2c1db7cce05b52198
Daniel Gomez <gomezvillegasdaniel@gmail.com>
2018-03-28 16:59:19 -0600

    add read a product by id

--- models/product.py
+++ models/product.py
@@ -19,6 +19,10 @@ class ProductModel(db.Model):
         self.price = price
     
     @classmethod
+    def find_by_id(cls, id):
+        return cls.query.filter_by(id=id).first()
+
+    @classmethod
     def find_by_name(cls, name):
         return cls.query.filter_by(name=name).first()
 
