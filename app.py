6171b1e15eba1d4947033c863a36d8bf2692ca1e
Daniel Gomez <gomezvillegasdaniel@gmail.com>
2018-03-28 16:04:37 -0600

    add database configuration

--- app.py
+++ app.py
@@ -5,8 +5,16 @@ from flask_restful import Api
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = os.environ.get('FLASK_SECRET_KEY')
 api = Api(app)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
     app.run(port=5000)
