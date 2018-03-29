a08609dc42e241417349beda0350e2257845db87
Daniel Gomez <gomezvillegasdaniel@gmail.com>
2018-03-29 14:10:14 -0600

    ref: pep8 refactoring to app.py

--- app.py
+++ app.py
@@ -12,9 +12,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = os.environ.get('FLASK_SECRET_KEY')
 api = Api(app)
 
+
 @app.before_first_request
 def create_tables():
     db.create_all()
+
 
 api.add_resource(Product, '/product', '/product/<int:id>')
 api.add_resource(ProductList, '/products')
@@ -22,5 +24,4 @@ api.add_resource(ProductList, '/products')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(port=5000,\
-        debug=False if os.environ.get('ENV') == 'PROD' else True)
+    app.run(port=5000, debug=False if os.environ.get('ENV') == 'PROD' else True)
