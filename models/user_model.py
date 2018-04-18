feaeaf7f4133893e6cb1cbcf085433a77993a647
Daniel Gomez <gomezvillegasdaniel@gmail.com>
2018-04-18 18:38:30 -0500

    add, ref: delete_from_db for user_model

--- models/user_model.py
+++ models/user_model.py
@@ -14,10 +14,6 @@ class UserModel(db.Model):
         self.password = password
         self.role = role
 
-    def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
-
     @classmethod
     def find_by_id(cls, _id):
         return cls.query.filter_by(id=_id).first()
@@ -29,3 +25,12 @@ class UserModel(db.Model):
     @classmethod
     def find_by_role(cls, role):
         return cls.query.filter_by(role=role).all()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+    
