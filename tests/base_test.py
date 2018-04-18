b15acb972baa10b9493dc9adffe4460a86c79815
Daniel Gomez <gomezvillegasdaniel@gmail.com>
2018-04-18 18:42:57 -0500

    add: base test

---
+++ tests/base_test.py
@@ -0,0 +1,25 @@
+import os
+from unittest import TestCase
+from app import app
+from db import db
+
+
+class BaseTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('TEST_DATABASE_URL')
+        app.config['DEBUG'] = False
+        with app.app_context():
+            db.init_app(app)
+
+    def setUp(self):
+        with app.app_context():
+            db.create_all()
+        self.app = app.test_client
+        self.app_context = app.app_context
+
+    def tearDown(self):
+        with app.app_context():
+            db.session.remove()
+            db.drop_all()
